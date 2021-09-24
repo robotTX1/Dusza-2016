2a8e2a96799deab4a0b0546167f785df43fa485e
Beni <benitx126@gmail.com>
2021-09-24 21:15:52 +0200

    HTML ProcessMD() alg test #1

--- src/com/dusza/HTML.java
+++ src/com/dusza/HTML.java
@@ -20,33 +20,61 @@ public class HTML {
 
 
         for (Paragraph p: input) {
+            boolean saveChange = false;
             boolean bold = false;
             boolean italic = false;
             boolean link = false;
             boolean paragraph = false;
-            String url = "";
+            StringBuilder url = new StringBuilder();
             String[] s = p.getFullText().split(" ");
 
+            StringBuilder charChainFragment = new StringBuilder();
             System.out.println(s);
+
+
 
             int i = 0;
             while (i < s.length)
             {
+                charChainFragment.append(s[i]);
                 if(s[i].substring(0,2).equals("**") && !bold) {
                     bold = true;
+                    saveChange = true;
 
                 } else if (s[i].charAt(0) == '*' && !italic) {
                     italic = true;
-                } else if (s[i].charAt(0) == '[' && !link)
+                    saveChange = true;
+                } else if (s[i].charAt(0) == '[' && !link) {
                     link = true;
+                    saveChange = true;
+                }
 
-                if(s[i].substring(s[i].length()-2, s[i].length()).equals("**") && !bold) {
-                    bold = true;
+                if(s[i].substring(s[i].length()-3, s[i].length()-1).equals("**") && bold) {
+                    bold = false;
+                    saveChange = true;
 
-                } else if (s[i].charAt(0) == '*' && !italic) {
-                    italic = true;
-                } else if (s[i].charAt(0) == '[' && !link)
-                    link = true;
+                } else if (s[i].charAt(s[i].length()-1) == '*' && italic) {
+                    italic = false;
+                    saveChange = true;
+                } else if (s[i].charAt(s[i].length()-1) == ']' && link) {
+                    link = false;
+                    saveChange = true;
+                    int index = i;
+                    while (s[index].charAt(s[index].length() - 1) != ')') {
+                        url.append(s[index]);
+                        index++;
+                    }
+                    i+= i-index-1;
+                }
+
+                if (saveChange) {
+                    p.addNewChainPiece(new CharacterChain(
+                            charChainFragment.toString(),italic, bold, link, url.toString()
+                            ));
+                    charChainFragment = new StringBuilder();
+                    saveChange = false;
+                }
+
                 i++;
             }
         }
