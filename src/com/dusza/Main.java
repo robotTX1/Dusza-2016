a90c0e73165539bf48daf3c0a7eded6b284c1bfd
Csaba <robottx3@gmail.com>
2021-09-24 20:43:52 +0200

    OutputHandler updated

--- src/com/dusza/Main.java
+++ src/com/dusza/Main.java
@@ -1,8 +1,26 @@
 package com.dusza;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static List<String> tags = new ArrayList<String>();
+
 
     public static void main(String[] args) {
-	// write your code here
+        Path inputPath = FileSystems.getDefault().getPath("input.txt");
+        Path outputPath = FileSystems.getDefault().getPath("output.html");
+
+        OutputHandler outputHandler = new OutputHandler(inputPath, outputPath);
+        List<Paragraph> paragraphList = outputHandler.readMarkdown();
+
+        for(Paragraph p : paragraphList) {
+            System.out.println(p.getFullText());
+        }
+
+
+
     }
 }
